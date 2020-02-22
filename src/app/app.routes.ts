deb1b7a1187e71cbafb25d95e7bc8a0c3428529e
Eze Gavilán <eze_gavilan@hotmail.com>
2020-02-22 20:36:35 -0300

    role guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,14 @@ import { HomeComponent } from './home/home/home.component';
 import { FormComponent } from './components/cliente/form.component';
 import { LoginComponent } from './components/usuarios/login.component';
 import { AuthGuard } from './guards/auth.guard';
+import { RoleGuard } from './guards/role.guard';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'clientes', component: ClienteComponent },
     { path: 'clientes/page/:page', component: ClienteComponent },
-    { path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard] },
-    { path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard] },
+    { path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: {role: 'ROLE_ADMIN'} },
+    { path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: {role: 'ROLE_ADMIN'} },
     { path: 'login', component: LoginComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
